af0a918f88e7300e2213ab13b6a1961b31becacb
Anderson Correa <andersonfpcorrea@gmail.com>
2023-06-06 00:35:41 -0300

    feat: create banner-grabber

--- banner_grabber.py
+++ banner_grabber.py
@@ -1,7 +1,7 @@
 import socket
 
 
-def banner(ip, port):
+def get_banner(ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((ip, port))
     s.settimeout(5)
@@ -12,7 +12,7 @@ def main():
     ip = input("Enter the IP address: ")
     port = int(input("Enter the port number: "))
     try:
-        banner(ip, port)
+        get_banner(ip, port)
     except (ConnectionRefusedError, TimeoutError) as e:
         print("Error: ", e)
 
